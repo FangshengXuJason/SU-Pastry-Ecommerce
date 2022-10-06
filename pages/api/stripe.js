4598b834de975cf7e481f394f243cc4d11be2907
FangshengXu <91645998+FangshengXuJason@users.noreply.github.com>
2022-10-06 11:25:17 -0700

    stripe.js: update the request send to stripe

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -11,16 +11,17 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
-          { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
+          { shipping_rate: 'shr_1LpwvlE6hJvmNXtbrpkjWpJf' },
+          { shipping_rate: 'shr_1LpwwqE6hJvmNXtbRhZHHY5b' },
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
-          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/vfxfwnaw/production/').replace('-webp', '.webp');
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/wfjiye4l/production/').replace('-webp', '.webp');
 
           return {
-            price_data: { 
+            price_data: {
               currency: 'usd',
-              product_data: { 
+              product_data: {
                 name: item.name,
                 images: [newImage],
               },
