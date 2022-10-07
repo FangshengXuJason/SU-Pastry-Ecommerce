2b7ef9460911a0e30bd445d3f600484bde30d0da
FangshengXu <91645998+FangshengXuJason@users.noreply.github.com>
2022-10-06 20:14:51 -0700

    feat: create canceled.js

---
+++ pages/canceled.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import Link from 'next/link';
+
+import { useStateContext } from '../context/StateContext';
+
+const Canceled = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+  useEffect(() => {
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantities(0);
+  }, []);
+
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <h2>You just cancelled your order</h2>
+        <Link href="/">
+          <button type="button" width="300px" className="btn">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Canceled
